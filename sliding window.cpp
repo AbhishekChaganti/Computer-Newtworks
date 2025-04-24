bfd59f1931704a5be25daf42e1debb5e68b8353a
AbhishekChaganti <158017910+AbhishekChaganti@users.noreply.github.com>
2025-04-24 22:55:09 +0530

    Update sliding window.cpp

--- sliding window.cpp
+++ sliding window.cpp
@@ -4,48 +4,58 @@
 #define WINDOW_SIZE 4
 #define TOTAL_FRAMES 10
 
-typedef
-struct { int
-frame_no;
-int is_acknowledged;
+typedef struct { 
+  int frame_no;
+  int is_acknowledged;
 } Frame;
 
-void send_frames(Frame frames[], int start, int
-window_size) { printf("Sending frames: ");
-for (int i = start; i < start + window_size && i < TOTAL_FRAMES; i++) {
-printf("%d ", frames[i].frame_no);
-}
-printf("\n");
-}
-
-void receive_ack(Frame frames[], int start, int window_size) {
-for (int i = start; i < start + window_size && i < TOTAL_FRAMES; i++) 
+void send_frames(Frame frames[], int start, int window_size) 
 { 
-if (rand() % 5 != 0) { // Simulating 80% success rate
-frames[i].is_acknowledged = 1;
-printf("Acknowledgment received for frame %d\n", frames[i].frame_no);
-} else {
-printf("Frame %d lost, needs retransmission\n", frames[i].frame_no);
+  printf("Sending frames: ");
+  for (int i = start; i < start + window_size && i < TOTAL_FRAMES; i++) 
+  {
+    printf("%d ", frames[i].frame_no);
+  }
+  printf("\n");
 }
-}}
 
-int main() {
-Frame frames[TOTAL_FRAMES];
-srand(time(0));
-for (int i = 0; i < TOTAL_FRAMES; i++) {
-frames[i].frame_no = i;
-frames[i].is_acknowledged = 0;
+void receive_ack(Frame frames[], int start, int window_size) 
+{
+  for (int i = start; i < start + window_size && i < TOTAL_FRAMES; i++) 
+  { 
+    if (rand() % 5 != 0) 
+    {
+      frames[i].is_acknowledged = 1;
+      printf("Acknowledgment received for frame %d\n", frames[i].frame_no);
+    } 
+    else 
+    {
+      printf("Frame %d lost, needs retransmission\n", frames[i].frame_no);
+    }
+  }
 }
-int base = 0;
-while (base < TOTAL_FRAMES) {
-send_frames(frames, base, WINDOW_SIZE);
-receive_ack(frames, base, WINDOW_SIZE);
-while (base < TOTAL_FRAMES && frames[base].is_acknowledged) {
-base++;
-}
-}
-printf("All frames successfully transmitted!\n");
-return 0;
+
+int main() 
+{
+  Frame frames[TOTAL_FRAMES];
+  srand(time(0));
+  for (int i = 0; i < TOTAL_FRAMES; i++) 
+  {
+    frames[i].frame_no = i;
+    frames[i].is_acknowledged = 0;
+  }
+  int base = 0;
+  while (base < TOTAL_FRAMES) 
+  {
+    send_frames(frames, base, WINDOW_SIZE);
+    receive_ack(frames, base, WINDOW_SIZE);
+    while (base < TOTAL_FRAMES && frames[base].is_acknowledged) 
+    {
+      base++;
+    }
+  }
+  printf("All frames successfully transmitted!\n");
+  return 0;
 }
 
 OUTPUT:
